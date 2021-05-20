082a0de0ed56ec24a39684f2723b310a76569c3d
Ankit Sangwan <ankit.sangwan.5688@gmail.com>
2021-05-20 11:55:32 +0530

    fixes #22

--- lib/recent.dart
+++ lib/recent.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:blackhole/audioplayer.dart';
@@ -18,7 +16,6 @@ class _RecentlyPlayedState extends State<RecentlyPlayed> {
   void getSongs() async {
     await Hive.openBox('recent');
     _songs = Hive.box('recent').get('recentlyPlayed');
-    print(_songs);
     added = true;
     setState(() {});
   }
@@ -59,49 +56,92 @@ class _RecentlyPlayedState extends State<RecentlyPlayed> {
                     : Theme.of(context).accentColor,
                 elevation: 0,
               ),
-              body: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  padding: EdgeInsets.only(top: 10, bottom: 10),
-                  shrinkWrap: true,
-                  itemCount: _songs.length,
-                  itemBuilder: (context, index) {
-                    return _songs.length == 0
-                        ? SizedBox()
-                        : ListTile(
-                            leading: Card(
-                              elevation: 5,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(7.0),
-                              ),
-                              clipBehavior: Clip.antiAlias,
-                              child: CachedNetworkImage(
-                                imageUrl: _songs[index]["image"]
-                                    .replaceAll('http:', 'https:'),
-                                placeholder: (context, url) => Image(
-                                  image: AssetImage('assets/cover.jpg'),
+              body: (_songs == null || _songs.length == 0)
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RotatedBox(
+                              quarterTurns: 3,
+                              child: Text(
+                                "Nothing to ",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Theme.of(context).accentColor,
+                                  fontWeight: FontWeight.w600,
                                 ),
                               ),
                             ),
-                            title:
-                                Text('${_songs[index]["title"].split("(")[0]}'),
-                            subtitle: Text(
-                                '${_songs[index]["artist"].split("(")[0]}'),
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                      opaque: false,
-                                      pageBuilder: (_, __, ___) => PlayScreen(
-                                            data: {
-                                              'response': _songs,
-                                              'index': index,
-                                              'offline': false,
-                                            },
-                                            fromMiniplayer: false,
-                                          )));
-                            },
-                          );
-                  }),
+                            Column(
+                              children: [
+                                Text(
+                                  "Show Here",
+                                  style: TextStyle(
+                                    fontSize: 50,
+                                    color: Theme.of(context).accentColor,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  "Go and Play Something",
+                                  style: TextStyle(
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  : ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      padding: EdgeInsets.only(top: 10, bottom: 10),
+                      shrinkWrap: true,
+                      itemCount: _songs.length,
+                      itemBuilder: (context, index) {
+                        return _songs.length == 0
+                            ? SizedBox()
+                            : ListTile(
+                                leading: Card(
+                                  elevation: 5,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(7.0),
+                                  ),
+                                  clipBehavior: Clip.antiAlias,
+                                  child: CachedNetworkImage(
+                                    imageUrl: _songs[index]["image"]
+                                        .replaceAll('http:', 'https:'),
+                                    placeholder: (context, url) => Image(
+                                      image: AssetImage('assets/cover.jpg'),
+                                    ),
+                                  ),
+                                ),
+                                title: Text(
+                                    '${_songs[index]["title"].split("(")[0]}'),
+                                subtitle: Text(
+                                    '${_songs[index]["artist"].split("(")[0]}'),
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      PageRouteBuilder(
+                                          opaque: false,
+                                          pageBuilder: (_, __, ___) =>
+                                              PlayScreen(
+                                                data: {
+                                                  'response': _songs,
+                                                  'index': index,
+                                                  'offline': false,
+                                                },
+                                                fromMiniplayer: false,
+                                              )));
+                                },
+                              );
+                      }),
             ),
           ),
           MiniPlayer(),
