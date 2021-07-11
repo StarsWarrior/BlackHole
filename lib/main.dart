0b4e1be7e4097e3f71b054dc765c02d6ee3d54f3
Surendrajat <surendrajat@protonmail.com>
2021-07-11 11:02:43 +0530

    keep song details cache size under 5MB

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,12 @@ void main() async {
     await Hive.openBox("recentlyPlayed");
   }
   try {
+    final box = await Hive.openBox('songDetails');
+    // clear box if it grows large
+    // each song detail is about 3.9KB so it's <5MB
+    if (box.length > 1200) {
+      box.clear();
+    }
     await Hive.openBox('songDetails');
   } catch (e) {
     print('Failed to open songDetails Box');
