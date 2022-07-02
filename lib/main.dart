051e058a47e3e362bfe78547094e45f341d500bd
Ankit Sangwan <ankit.sangwan.5688@gmail.com>
2022-07-02 16:27:40 +0530

    fixed issue of app not opening

--- lib/main.dart
+++ lib/main.dart
@@ -200,7 +200,7 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget initialFuntion() {
-    return Hive.box('settings').get('userId') == null
+    return Hive.box('settings').get('userId') != null
         ? HomePage()
         : AuthScreen();
   }
